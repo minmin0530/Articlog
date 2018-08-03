63e53d73dbe6cd2b7a5d271f6415e729737af655
izumi_yoshiki <izumiyoshiki@icloud.com>
2018-08-03 11:35:58 +0900

    decolate linkCard

--- home/home.js
+++ home/home.js
@@ -104,11 +104,13 @@ class Home {
       const linkCard = document.createElement('div');
       linkCard.style.position = "absolute";
       linkCard.style.top = "400px";
-      linkCard.style.left = "400px";
+      linkCard.style.left = "600px";
       linkCard.style.width = "400px";
       linkCard.style.height = "200px";
-      linkCard.style.background = "#aaa";
-      linkCard.innerHTML = "<a href='https://articlog.com/" + data.link + "'>" + data.title + "</a>";
+      linkCard.style.background = "#fff";
+      linkCard.style.border = "solid #008 5px";
+      linkCard.style.padding = "50px";
+      linkCard.innerHTML = "公開できました。<br><a href='https://articlog.com/" + data.link + "'>" + data.title + "</a>";
 
       document.body.appendChild(linkCard);
     }); 
