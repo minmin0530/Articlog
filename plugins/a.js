bbc3e6e86d1e896372204d34d47784769a7b7650
izumi_yoshiki <izumiyoshiki@icloud.com>
2018-08-15 13:33:42 +0900

    plugin use db

--- plugins/a.js
+++ plugins/a.js
@@ -3,11 +3,14 @@ class PluginTest1 {
   }
   print(MongoClient, url, dbName, fs, __dirname, hoge) {
 
+    console.log('print');
     let result = '';
     MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {     
       const db = client.db(dbName);
       const collection = db.collection('article');
+      console.log('mongodb');
       collection.find({}).toArray( (err, docs) => {
+        console.log(docs.length);
         for (const doc of docs) {
           // app.get('/' + doc.link.substring(0, doc.link.length - 5), (req, res) => {
           //   res.sendFile(__dirname + '/html/' + doc.link);
@@ -28,7 +31,7 @@ class PluginTest1 {
 
 
 
-    return 'plugin-a:' + result;
+//    return 'plugin-a:' + result;
   }
 };
 
