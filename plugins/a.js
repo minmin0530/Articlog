d7f81db322d342ad9be5a7a47f518e029bd28788
izumi_yoshiki <izumiyoshiki@icloud.com>
2018-08-15 14:04:36 +0900

    plugin dispalay async await

--- plugins/a.js
+++ plugins/a.js
@@ -3,41 +3,46 @@ class PluginTest1 {
   }
   print(MongoClient, url, dbName, fs, __dirname, hoge) {
 
-    let result = '0';
-    MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {     
-      const db = client.db(dbName);
-      const collection = db.collection('article');
-      result += '1';
-      collection.find({}).toArray( (err, docs) => {
-        result += '2';
-        for (const doc of docs) {
-          // app.get('/' + doc.link.substring(0, doc.link.length - 5), (req, res) => {
-          //   res.sendFile(__dirname + '/html/' + doc.link);
-          // });
-          result += '3';
-
-          return doc.link;
-
-          // fs.readFile(__dirname + '/html/' + doc.link, (err, data) => {
-          //   //result += data;
-          //   return data;
-          // });
-      
-
-
-        }
-        result += '4';
-
-      });
-      result += '5';
-
-    });
+    async function returnHTML() {
+      let result = '0';
+      await MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {     
+        const db = client.db(dbName);
+        const collection = db.collection('article');
+        result += '1';
+        collection.find({}).toArray( (err, docs) => {
+          result += '2';
+          for (const doc of docs) {
+            // app.get('/' + doc.link.substring(0, doc.link.length - 5), (req, res) => {
+            //   res.sendFile(__dirname + '/html/' + doc.link);
+            // });
+            result += '3';
   
-
-
-
-
-    return 'plugin-a:' + result;
+            return doc.link;
+  
+            // fs.readFile(__dirname + '/html/' + doc.link, (err, data) => {
+            //   //result += data;
+            //   return data;
+            // });
+        
+  
+  
+          }
+          result += '4';
+          return result;
+  
+        });
+        result += '5';
+        return result;
+  
+      });
+    
+  
+  
+  
+  
+      return 'plugin-a:' + result;
+      }
+    return returnHTML();
   }
 };
 
