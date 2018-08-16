5f82a00c2906dc1eae95517a30c1fbbae3dd9d25
izumi_yoshiki <izumiyoshiki@icloud.com>
2018-08-16 17:41:00 +0900

    Date().toLocalString => Date().toLocalString('ja')

--- plugins/a.js
+++ plugins/a.js
@@ -22,74 +22,18 @@ class PluginTest1 {
         html      = await fs.readFileSync(__dirname + '/html/' + doc.link);
         begin     = html.toString().indexOf('<section>');
         end       = html.toString().indexOf('</section>') + 10;
-        result   += end;//doc.time;
+        result   += doc.time;
         result   += html.toString().substring(begin, end);
       }
+      result += new Date().toLocaleString("ja");
       end = html.toString().indexOf('</article>');
-      result += end;
       const begin_html = html.toString().substring(0, begin);
       const end_html = html.toString().substring(end, html.length);
     
       //DB切断
       await client.close()
-    
-
-
-//      const html = await fs.readFileSync(__dirname + '/html/' + result);
-
-      return begin_html + result + end_html;//data.map(x => { //何かする })
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-      let result = '0';
-      await MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {     
-        const db = client.db(dbName);
-        const collection = db.collection('article');
-        result += '1';
-        collection.find({}).toArray( (err, docs) => {
-          result += '2';
-          for (const doc of docs) {
-            // app.get('/' + doc.link.substring(0, doc.link.length - 5), (req, res) => {
-            //   res.sendFile(__dirname + '/html/' + doc.link);
-            // });
-            result += '3';
-  
-            return doc.link;
-  
-            // fs.readFile(__dirname + '/html/' + doc.link, (err, data) => {
-            //   //result += data;
-            //   return data;
-            // });
-        
-  
-  
-          }
-          result += '4';
-          return result;
-  
-        });
-        result += '5';
-        return result;
-  
-      });
-    
-  */
-  
-  
-  
-///      return 'plugin-a:' + result;
-      }
+      return begin_html + result + end_html;
+    }
     return await returnHTML();
   }
 };
